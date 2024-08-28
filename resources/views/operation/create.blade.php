b0caf5cc7546f111fe8734fd86ad030dc6b4ec42
Jesus Garcia <j.garcia@bambelo.com>
2024-08-28 11:36:25 +0200

    feat(refactor): refactor controllers to move logic to a service layer

--- resources/views/operation/create.blade.php
+++ resources/views/operation/create.blade.php
@@ -128,7 +128,7 @@
                                                    text-gray-900 ring-1 ring-inset ring-gray-300
                                                    placeholder:text-gray-400 focus:ring-2 focus:ring-inset
                                                    focus:ring-indigo-600 sm:text-sm sm:leading-6
-                                                   @error('operand1')
+                                                   @error('operand2')
                                                     text-red-600 ring-red-300
                                                    @else
                                                     text-gray-900 ring-gray-300
